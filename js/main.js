6916bcd60868850d2ded2a81eeb25ef9d58de2c6
Boti05 <erdelyi.botond1@gmail.com>
2024-01-27 22:40:54 +0100

    select-info
    Creating a game preset selector and creatin an info button with little animation that tells the informations.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function show_info() {
+    const paragraph = document.getElementById("info_text");
+    paragraph.innerHTML = "informácio";
+}
+
+function hide_info() {
+    const paragraph = document.getElementById("info_text");
+    paragraph.innerHTML = "Info"
+}
